418b0434eb2c6ed11a3e8a486ee56219d486514b
ArslanHaq <arslankhanhaq332@gmail.com>
2024-09-27 17:13:02 +0500

    first commit

---
+++ src/components/Contact/index.tsx
@@ -0,0 +1,172 @@
+"use client"
+import { useState } from "react";
+import NewsLatterBox from "./NewsLatterBox";
+
+const emailUrl = 'http://192.168.100.123:5015/Email'
+const Contact = () => {
+
+  const [formValues, setFormValues] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  })
+  const handleFormSubmit = async (e: any) => {
+    e.preventDefault();
+
+    console.log(formValues); // Log the form values to verify
+
+    try {
+      const response = await fetch(emailUrl, {
+        method: 'POST', // Set the request method to POST
+        headers: {
+          'Content-Type': 'application/json', // Specify that you're sending JSON
+        },
+        body: JSON.stringify(formValues), // Convert formValues to a JSON string
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      console.log('Email sent successfully');
+      setFormValues({
+        email: '',
+        message: '',
+        name: "",
+        subject: ''
+      })
+    } catch (error) {
+      console.error('Error sending email:', error);
+    }
+  };
+  return (
+    <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
+      <div className="container">
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4 ">
+            <div
+              className="mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
+              data-wow-delay=".15s
+              "
+            >
+              <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
+                Need Help?
+              </h2>
+              <p className="mb-12 text-base font-medium text-body-color">
+                Our support team will get back to you ASAP via email.
+              </p>
+              <form onSubmit={handleFormSubmit}>
+                <div className="-mx-4 flex flex-wrap">
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="name"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your Name
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="Enter your name"
+                        value={formValues.name}
+                        onChange={(e) => {
+                          setFormValues({
+                            ...formValues,
+                            name: e.target.value,
+                          });
+                        }}
+                        required
+                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="email"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your Email
+                      </label>
+                      <input
+                        type="email"
+                        value={formValues.email}
+                        onChange={(e) => {
+                          setFormValues({
+                            ...formValues,
+                            email: e.target.value,
+                          });
+                        }}
+                        required
+                        placeholder="Enter your email"
+                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="email"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Subject
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="Enter your subject"
+                        value={formValues.subject}
+                        onChange={(e) => {
+                          setFormValues({
+                            ...formValues,
+                            subject: e.target.value,
+                          });
+                        }}
+                        required
+                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="message"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your Message
+                      </label>
+                      <textarea
+                        name="message"
+                        rows={5}
+                        placeholder="Enter your Message"
+                        value={formValues.message}
+                        onChange={(e) => {
+                          setFormValues({
+                            ...formValues,
+                            message: e.target.value,
+                          });
+                        }}
+                        required
+                        className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
+                      Submit Ticket
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          {/* <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
+            <NewsLatterBox />
+          </div> */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
